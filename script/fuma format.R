54b79569c9790671e1d64fd10ba2c1aefc820e87
annabeth0218 <annabeth0218@gmail.com>
2024-11-23 14:22:48 +0800

    Script

--- script/fuma format.R
+++ script/fuma format.R
@@ -4,21 +4,9 @@ library(BSgenome.Hsapiens.UCSC.hg38) # using GRCh38/hg38
 library(tidyverse)
 
 # import
-gwas.dep <- read.csv('mtag_gwas_snp/onesample/gwas/depression_log_ADD.csv')
-gwas.ded <- read.csv('mtag_gwas_snp/onesample/gwas/dryeye_case60_log_ADD.csv')
-gwas.ibs <- read.csv('mtag_gwas_snp/onesample/gwas/ibs_case50_log_ADD.csv')
-
-# mtag.dep <- read.csv('mtag_gwas_snp/onesample/mtag/one_mtag_dep.csv')
-# mtag.ded <- read.csv('mtag_gwas_snp/onesample/mtag/one_mtag_dry60.csv')
-# mtag.ibs <- read.csv('mtag_gwas_snp/onesample/mtag/one_mtag_ibs50.csv')
-
-# gwas.dep.two <- read.csv('mtag_gwas_snp/twosample/gwas/two_dep_log_ADD.csv')
-# gwas.ded.two <- read.csv('mtag_gwas_snp/twosample/gwas/two_dry_case60_log_ADD.csv')
-# gwas.ibs.two <- read.csv('mtag_gwas_snp/twosample/gwas/two_ibs_case50_log_ADD.csv')
-# 
-# mtag.dep.two <- read.csv('mtag_gwas_snp/twosample/mtag/two_mtag_dep.csv')
-# mtag.ded.two <- read.csv('mtag_gwas_snp/twosample/mtag/two_mtag_dry60.csv')
-# mtag.ibs.two <- read.csv('mtag_gwas_snp/twosample/mtag/two_mtag_ibs50.csv')
+gwas.dep <- read.csv('gwas_snp/with_A2/one_sample_gwas/depression_log.csv')
+gwas.ded <- read.csv('gwas_snp/with_A2/one_sample_gwas/dryeye_case60_log.csv')
+gwas.ibs <- read.csv('gwas_snp/with_A2/one_sample_gwas/ibs_case50_log.csv')
 
 tp <- mtag.dep.two |>
   filter(str_detect(SNP, "^rs")) |>
