2a7a43eea9cccaefad1b903aa71b459ca6804232
annabeth0218 <annabeth0218@gmail.com>
2025-07-02 17:30:09 +0800

    moving papers

--- script/venn.R
+++ script/venn.R
@@ -1,11 +1,20 @@
+# ggplot basics
 # tutorial https://www.datanovia.com/en/blog/venn-diagram-with-r-or-rstudio-a-million-ways/
+# Show a list of available shapes
+df_shapes <- data.frame(shape = 0:24)
+ggplot(df_shapes, aes(0, 0, shape = shape)) +
+  geom_point(aes(shape = shape), size = 5, fill = 'red') +
+  scale_shape_identity() +
+  facet_wrap(~shape) +
+  theme_void()
+
+# venn
 library(ggvenn)
 library(data.table)
 
 ibs <- read.table("/Volumes/256_Anna/3MR/ibs_case50_new_QC2.bim", header = FALSE)
 ded <- read.table("/Volumes/256_Anna/3MR/dryeye_case60_new_QC2.bim", header = FALSE)
 dep <- read.table("/Volumes/256_Anna/3MR/depression_new_QC2.bim", header = FALSE)
-
 
 venn2 <- list(
   FUMA = fuma_genes.ibs$ensg,
@@ -30,6 +39,3 @@ ggvenn(
   fill_color = c("#0073C2FF", "#CD534CFF", "#868686FF"),
   stroke_size = 0.5, set_name_size = 5, text_size = 2
 )
-
-
-
