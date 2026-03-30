34bb1fddc88dc670314708a1d0cae3e63d77e14e
annabeth0218 <annabeth0218@gmail.com>
2026-03-30 15:48:52 +0800

    after 大專生 submitted

--- script/environment.R
+++ script/environment.R
@@ -12,3 +12,18 @@ gwas.ibs <- read.table('table/gwas_snp/with_A2/one_sample_gwas/hg19_gwas_ibs.txt
 magma_ibs <- read.delim("fuma/fuma_ibs/magma.genes.out") |> filter(P < 0.05)
 magma_ded <- read.delim("fuma/fuma_ded/magma.genes.out") |> filter(P < 0.05)
 magma_dep <- read.delim("fuma/fuma_dep/magma.genes.out") |> filter(P < 0.05)
+
+snps.ded <- read.table("fuma/fuma_ded/snps.txt", header = T)
+snps.dep <- read.table("fuma/fuma_dep/snps.txt", header = T)
+snps.ibs <- read.table("fuma/fuma_ibs/snps.txt", header = T)
+
+riskloci.ded <- read.table("fuma/fuma_ded/IndSigSNPs.txt", header = T)
+riskloci.dep <- read.table("fuma/fuma_dep/IndSigSNPs.txt", header = T)
+riskloci.ibs <- read.table("fuma/fuma_ibs/IndSigSNPs.txt", header = T)
+
+df <- snps.ded[snps.ded$rsID %in% riskloci.ded$rsID, ]
+write.table(df, file = "table/Riskloci_ded.txt", sep = "\t", row.names = FALSE, quote = FALSE)
+df <- snps.dep[snps.dep$rsID %in% riskloci.dep$rsID, ]
+write.table(df, file = "table/Riskloci_dep.txt", sep = "\t", row.names = FALSE, quote = FALSE)
+df <- snps.ibs[snps.ibs$rsID %in% riskloci.ibs$rsID, ]
+write.table(df, file = "table/Riskloci_ibs.txt", sep = "\t", row.names = FALSE, quote = FALSE)
--- script/template.R
+++ script/template.R
@@ -8,6 +8,7 @@ ggplot(data = X2sample_CoJo, aes(x = n , y = m ,fill = group))+
   labs(x="number of sample", y="-log(P value)")+
   scale_fill_manual(values = c( "#FFC900", "#FF9000", "#22D222", "#005800", "#D879FF", "#7100A4"))
 
+# jpgs/pngs to pdf
 library(magick)
 img_dir <- "/Users/annabethlu/jpgs"
 files <- list.files(img_dir, pattern = "\\.(jpe?g|png)$", full.names = TRUE, ignore.case = TRUE)
@@ -16,6 +17,27 @@ files <- list.files(img_dir, pattern = "\\.(jpe?g|png)$", full.names = TRUE, ign
 # install.packages("gtools")
 # files <- gtools::mixedsort(files)
 
-imgs <- image_read(files)
+imgs <- image_read(files) |> image_convert(format = "pdf", quality = 60)
 image_write(imgs, path = "/Users/annabethlu/jpgs/output.pdf", format = "pdf")
+
+# pdf to png
+pdf_dir <- 'figs/fig2.pdf'
+pdf_convert(pdf_dir, format = "png", dpi = 300)
+
+# png to pdf (high resolution)
+img <- image_read("input.png")
+img_a4 <- image_resize(img, "5000")
+image_write(img_a4, path = "output.pdf", format = "pdf", density = 300)
+
+# compress pics.pdf
+input <- image_read_pdf("/Users/annabethlu/jpgs/1_handout.pdf")
+out <- image_convert(input, format = "pdf", quality = 60)
+image_write(image_join(out), path = "/Users/annabethlu/jpgs/smaller_file.pdf")
+
+# merge pdfs
+library(qpdf)
+pdf_a <- '/Users/annabethlu/jpgs/1_handout.pdf' 
+pdf_b <- '/Users/annabethlu/jpgs/output.pdf'
+pdf_combine(input = c(pdf_a, pdf_b), output = "merged.pdf")
+
 
